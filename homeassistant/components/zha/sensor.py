daa096966bc4caafeaccd44537f413b1bb6c8cac
Magnus Stattin <stattin42@gmail.com>
2022-10-25 09:22:27 +0200

    Disable polling for ElectricalMeasurement and SmartEnergyMetering sensors. Rely on device reporting based on attribute reporting configuration.

--- homeassistant/components/zha/sensor.py
+++ homeassistant/components/zha/sensor.py
@@ -256,7 +256,7 @@ class ElectricalMeasurement(Sensor):
 
     SENSOR_ATTR = "active_power"
     _attr_device_class: SensorDeviceClass = SensorDeviceClass.POWER
-    _attr_should_poll = True  # BaseZhaEntity defaults to False
+    _attr_should_poll = False  # Don't poll. Rely on device reporting. (BaseZhaEntity defaults to False)
     _attr_state_class: SensorStateClass = SensorStateClass.MEASUREMENT
     _attr_name: str = "Active power"
     _attr_native_unit_of_measurement: str = UnitOfPower.WATT
@@ -499,7 +499,7 @@ class SmartEnergySummation(SmartEnergyMetering, id_suffix="summation_delivered")
 class PolledSmartEnergySummation(SmartEnergySummation):
     """Polled Smart Energy Metering summation sensor."""
 
-    _attr_should_poll = True  # BaseZhaEntity defaults to False
+    _attr_should_poll = False  # Don't poll. Rely on device reporting. (BaseZhaEntity defaults to False)
 
     async def async_update(self) -> None:
         """Retrieve latest state."""
